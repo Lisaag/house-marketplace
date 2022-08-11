1826c27656300edbead1155a4bd2e483141088e9
Lisaag <lisatgroen@hotmail.com>
2022-08-11 11:04:52 +0900

    explore page and rent, sale pages with listings using firestore

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navbar from './components/Navbar'
 import PrivateRoute from './components/PrivateRoute'
 
 import Explore from './pages/Explore'
+import Category from './pages/Category'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
@@ -24,6 +25,10 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Explore />} />
 					<Route path='/offers' element={<Offers />} />
+					<Route
+						path='/category/:categoryName'
+						element={<Category />}
+					></Route>
 					<Route path='/profile' element={<PrivateRoute />}>
 						<Route path='/profile' element={<Profile />} />
 					</Route>
