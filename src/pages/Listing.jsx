912f9fdc969129ec4bd4e8897e6044e9ae31c96c
Lisaag <lisatgroen@hotmail.com>
2022-08-27 12:30:33 +0900

    Explore slider on main menu and image slider on house pages
    using Swiper

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,10 +1,21 @@
 import { useState, useEffect } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
+import SwiperCore, {
+	Navigation,
+	Pagination,
+	Scrollbar,
+	A11y,
+	Autoplay,
+} from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay])
 
 function Listing() {
 	const [listing, setListing] = useState(null)
@@ -14,6 +25,8 @@ function Listing() {
 	const navigate = useNavigate()
 	const params = useParams()
 	const auth = getAuth()
+
+	let swiperDelay = 4500
 
 	useEffect(() => {
 		const fetchListing = async () => {
@@ -35,6 +48,27 @@ function Listing() {
 
 	return (
 		<main>
+			<Swiper
+				autoplay={{
+					delay: swiperDelay,
+					disableOnInteraction: false,
+				}}
+				slidesPerView={1}
+				pagination={{ clickable: true }}
+			>
+				{listing.imgUrls.map((url, index) => (
+					<SwiperSlide key={index}>
+						<div
+							style={{
+								background: `url(${listing.imgUrls[index]})
+								 center no-repeat`,
+								backgroundSize: 'cover',
+							}}
+							className='swiperSlideDiv'
+						></div>
+					</SwiperSlide>
+				))}
+			</Swiper>
 			<div
 				className='shareIconDiv'
 				onClick={() => {
@@ -86,7 +120,30 @@ function Listing() {
 					<li>{listing.furnished && 'Furnished'}</li>
 				</ul>
 				<p className='listingLocationTitle'>Location</p>
-				{/* MAP */}
+				<div className='leafletContainer'>
+					<MapContainer
+						style={{ height: '100%', width: '100%' }}
+						center={[
+							listing.geolocation.lat,
+							listing.geolocation.lng,
+						]}
+						zoom={13}
+						scrollWheelZoom={true}
+					>
+						<TileLayer
+							attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+							url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+						/>
+						<Marker
+							position={[
+								listing.geolocation.lat,
+								listing.geolocation.lng,
+							]}
+						>
+							<Popup>{listing.location}</Popup>
+						</Marker>
+					</MapContainer>
+				</div>
 
 				{auth.currentUser?.uid !== listing.userRef && (
 					<Link
